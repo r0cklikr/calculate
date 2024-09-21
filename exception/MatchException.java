a6cee66eca0f66f85537dafb616070cd98ff24cd
rocklikr <132861473+r0cklikr@users.noreply.github.com>
2024-09-21 12:46:47 +0800

    Add files via upload

---
+++ exception/MatchException.java
@@ -0,0 +1,8 @@
+package com.rock.ljjkr.exception;
+
+public class MatchException extends RuntimeException{
+    public MatchException(){}
+    public MatchException(String message){
+        super(message);
+    }
+}
---
+++ exception/MathException.java
@@ -0,0 +1,8 @@
+package com.rock.ljjkr.exception;
+
+public class MathException extends RuntimeException{
+    public MathException(){}
+    public MathException(String message){
+        super(message);
+    }
+}
